0cbe182d33804748ef829b39b7c472392e8d5f72
alessandroquirile <alessandro.quirile@outlook.com>
2025-10-14 10:44:40 +0200

    General minor refactoring

---
+++ src/main.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Tommaso Giacometti
+"""
+import argparse
+import configparser
+import random
+import time
+
+import matplotlib.pyplot as plt
+
+import plots
+import utils
+from protein_class import Protein
+
+# Parser to get from terminal the configuration file
+parser = argparse.ArgumentParser()
+# the filename is optional, the default is the config.txt
+parser.add_argument('configuration_file', help='file from which takes the configuration', default='config.txt',
+                    nargs='?')
+
+args = parser.parse_args()  # get the args written in the terminal
+filename = args.configuration_file  # assign filename
+
+# setting the parameters from configuration file
+configuration = configparser.ConfigParser()
+configuration.read(filename)
+
+config = utils.Configuration(configuration)  # class to get the save the configuration from the file
+
+# Random seed setting
+random.seed(config.seed)
+print(f'The random seed used is {config.seed}')
+
+prot = Protein(config)  #  Protein class
+
+plots.view(protein=prot, tit='Initial configuration')  # plot the structure of the protein
+
+start = time.time()
+
+print('--------------------')
+print('Evolution started...')
+prot.evolution()  # evolve the protein with folds foldings
+print('Evolution ended')
+print('---------------')
+
+# various plots:
+plots.view(protein=prot, save=False, tit='Final configuration')
+plots.view_min_en(protein=prot)
+plots.view_max_comp(protein=prot)
+plots.plot_energy(protein=prot, avg=10)
+plots.plot_compactness(protein=prot, avg=10)
+
+print(f'It took {time.time() - start:.3f} seconds')
+
+plt.show()  # to let the let plots on screen at the end
+
+if config.gif:
+    plots.create_gif(protein=prot)
